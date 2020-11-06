5c904d5f60b034a0e1ddce846ae828005a533507
Ander Gray <ander.gray@liverpool.ac.uk>
2020-11-06 03:11:01 +0000

    adds sub script

--- oktav_fe/runEndf.sh
+++ oktav_fe/runEndf.sh
@@ -1,1 +1,1 @@
-run_TMC -s 500 -l ../randomData/endf_rand/cross_sections_sandy.xml -name endfFe -n C0 Fe54 Fe56 Fe57 Si28 Si29 Si30 O16
+run_TMC.py -s 500 -l ../randomData/endf_rand/cross_sections_sandy.xml -name endfFe -n C0 Fe54 Fe56 Fe57 Si28 Si29 Si30 O16
--- oktav_ni/runEndf.sh
+++ oktav_ni/runEndf.sh
@@ -1,1 +1,1 @@
-run_TMC -s 500 -l ../randomData/endf_rand/cross_sections_sandy.xml -name endfNi -n C0 Fe54 Fe56 Fe57 Si28 Si29 Si30 O16 Ni58 Ni60 H1 B10 B11 Mg24 Mg25 Mg26
+run_TMC.py -s 500 -l ../randomData/endf_rand/cross_sections_sandy.xml -name endfNi -n C0 Fe54 Fe56 Fe57 Si28 Si29 Si30 O16 Ni58 Ni60 H1 B10 B11 Mg24 Mg25 Mg26
